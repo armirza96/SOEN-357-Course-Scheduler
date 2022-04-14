c0f2fbe28ba74ab1c2287e0f48f3f275d83feaa6
Abdul-Rahman Mirza <abdul_mirza805@hotmail.com>
2022-04-14 14:24:03 -0400

    commit for vuetify install
    comming for ahnaf so that he can have veutify as well

--- course-scheduler/src/components/HelloWorld.vue
+++ course-scheduler/src/components/HelloWorld.vue
@@ -1,58 +1,382 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div>
+ <v-row>
+    <v-col>
+      <div class="rounded-l-xl rounded-r-xl " style="height: 50px; background-color: #d6c5b3; overflow: hidden;"> 
+        <span style="background-color: #58513f; height: 100%; width: fit-content; color: #d6c5b3" class="d-inline-flex align-center pa-2">
+          Courses
+        </span>
+        <span style="height: 100%; width: fit-content; color: #58513f" class="d-inline-flex align-center pa-2">
+          SOEN 345 |
+        </span>
+        <v-btn icon color="light-brown" class="float-right pa-2 mx-auto my-auto d-inline-flex align-center h-100" @click="changeState(true)">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+          
+      </div>
+    </v-col>
+  </v-row>
+  <v-row v-if="showForm" class="mt-0">
+    <v-col>
+      <div style="width: 50%; background-color:#d6c5b3; border-radius: 15px;" class="float-right pa-4">
+        <validation-observer
+    ref="observer"
+    v-slot="{ invalid }">
+    <form @submit.prevent="submit">
+      <validation-provider
+        v-slot="{ errors }"
+        name="Name"
+        rules="required|max:10"
+      >
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :error-messages="errors"
+          label="Name"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        name="phoneNumber"
+        :rules="{
+          required: true,
+          digits: 7,
+          regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$'
+        }"
+      >
+        <v-text-field
+          v-model="phoneNumber"
+          :counter="7"
+          :error-messages="errors"
+          label="Phone Number"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        name="email"
+        rules="required|email"
+      >
+        <v-text-field
+          v-model="email"
+          :error-messages="errors"
+          label="E-mail"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        name="select"
+        rules="required"
+      >
+        <v-select
+          v-model="select"
+          :items="items"
+          :error-messages="errors"
+          label="Select"
+          data-vv-name="select"
+          required
+        ></v-select>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required"
+        name="checkbox"
+      >
+        <v-checkbox
+          v-model="checkbox"
+          :error-messages="errors"
+          value="1"
+          label="Option"
+          type="checkbox"
+          required
+        ></v-checkbox>
+      </validation-provider>
+
+      <v-btn
+        class="mr-4"
+        type="submit"
+        :disabled="invalid"
+      >
+        submit
+      </v-btn>
+      <v-btn @click="clear">
+        clear
+      </v-btn>
+    </form>
+  </validation-observer>
+      </div>
+    </v-col>
+  </v-row>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="100%">
+        <v-calendar
+          ref="calendar"
+          v-model="value"
+          color="primary"
+          type="week"
+          :events="events"
+          :event-color="getEventColor"
+          :event-ripple="false"
+          @change="getEvents"
+          @mousedown:event="startDrag"
+          @mousedown:time="startTime"
+          @mousemove:time="mouseMove"
+          @mouseup:time="endDrag"
+          @mouseleave.native="cancelDrag"
+        >
+          <template v-slot:event="{ event, timed, eventSummary }">
+            <div
+              class="v-event-draggable"
+              v-html="eventSummary()"
+            ></div>
+            <div
+              v-if="timed"
+              class="v-event-drag-bottom"
+              @mousedown.stop="extendBottom(event)"
+            ></div>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</div>
+ 
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+  setInteractionMode('eager')
+
+  extend('digits', {
+    ...digits,
+    message: '{_field_} needs to be {length} digits. ({_value_})',
+  })
+    extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+
+  extend('max', {
+    ...max,
+    message: '{_field_} may not be greater than {length} characters',
+  })
+
+  extend('regex', {
+    ...regex,
+    message: '{_field_} {_value_} does not match {regex}',
+  })
+
+  extend('email', {
+    ...email,
+    message: 'Email must be valid',
+  })
+
+  export default {
+    name: 'HelloWorld',
+      components: {
+      ValidationProvider,
+      ValidationObserver,
+    },
+    data: () => ({
+     value: '',
+      events: [],
+      colors: ['#2196F3', '#3F51B5', '#673AB7', '#00BCD4', '#4CAF50', '#FF9800', '#757575'],
+      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+      dragEvent: null,
+      dragStart: null,
+      createEvent: null,
+      createStart: null,
+      extendOriginal: null,
+      showForm: true,
+      courseName: '',
+      courseCredits: '',
+      difficultyLevel: '',
+      minimumHours: '',
+      select: null,
+      items: [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+      ],
+      courseStartTime: null,
+      courseEndTime: null
+    }),
+    methods: {
+      changeState(showForm) {
+        this.showForm = showForm;
+      },
+      startDrag ({ event, timed }) {
+        if (event && timed) {
+          this.dragEvent = event
+          this.dragTime = null
+          this.extendOriginal = null
+        }
+      },
+      startTime (tms) {
+        const mouse = this.toTime(tms)
+        if (this.dragEvent && this.dragTime === null) {
+          const start = this.dragEvent.start
+          this.dragTime = mouse - start
+        } else {
+          this.createStart = this.roundTime(mouse)
+          this.createEvent = {
+            name: `Event #${this.events.length}`,
+            color: this.rndElement(this.colors),
+            start: this.createStart,
+            end: this.createStart,
+            timed: true,
+          }
+          this.events.push(this.createEvent)
+        }
+      },
+      extendBottom (event) {
+        this.createEvent = event
+        this.createStart = event.start
+        this.extendOriginal = event.end
+      },
+      mouseMove (tms) {
+        const mouse = this.toTime(tms)
+        if (this.dragEvent && this.dragTime !== null) {
+          const start = this.dragEvent.start
+          const end = this.dragEvent.end
+          const duration = end - start
+          const newStartTime = mouse - this.dragTime
+          const newStart = this.roundTime(newStartTime)
+          const newEnd = newStart + duration
+          this.dragEvent.start = newStart
+          this.dragEvent.end = newEnd
+        } else if (this.createEvent && this.createStart !== null) {
+          const mouseRounded = this.roundTime(mouse, false)
+          const min = Math.min(mouseRounded, this.createStart)
+          const max = Math.max(mouseRounded, this.createStart)
+          this.createEvent.start = min
+          this.createEvent.end = max
+        }
+      },
+      endDrag () {
+        this.dragTime = null
+        this.dragEvent = null
+        this.createEvent = null
+        this.createStart = null
+        this.extendOriginal = null
+      },
+      cancelDrag () {
+        if (this.createEvent) {
+          if (this.extendOriginal) {
+            this.createEvent.end = this.extendOriginal
+          } else {
+            const i = this.events.indexOf(this.createEvent)
+            if (i !== -1) {
+              this.events.splice(i, 1)
+            }
+          }
+        }
+        this.createEvent = null
+        this.createStart = null
+        this.dragTime = null
+        this.dragEvent = null
+      },
+      roundTime (time, down = true) {
+        const roundTo = 15 // minutes
+        const roundDownTime = roundTo * 60 * 1000
+        return down
+          ? time - time % roundDownTime
+          : time + (roundDownTime - (time % roundDownTime))
+      },
+      toTime (tms) {
+        return new Date(tms.year, tms.month - 1, tms.day, tms.hour, tms.minute).getTime()
+      },
+      getEventColor (event) {
+        const rgb = parseInt(event.color.substring(1), 16)
+        const r = (rgb >> 16) & 0xFF
+        const g = (rgb >> 8) & 0xFF
+        const b = (rgb >> 0) & 0xFF
+        return event === this.dragEvent
+          ? `rgba(${r}, ${g}, ${b}, 0.7)`
+          : event === this.createEvent
+            ? `rgba(${r}, ${g}, ${b}, 0.7)`
+            : event.color
+      },
+      getEvents ({ start, end }) {
+        const events = []
+        const min = new Date(`${start.date}T00:00:00`).getTime()
+        const max = new Date(`${end.date}T23:59:59`).getTime()
+        const days = (max - min) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+        for (let i = 0; i < eventCount; i++) {
+          const timed = this.rnd(0, 3) !== 0
+          const firstTimestamp = this.rnd(min, max)
+          const secondTimestamp = this.rnd(2, timed ? 8 : 288) * 900000
+          const start = firstTimestamp - (firstTimestamp % 900000)
+          const end = start + secondTimestamp
+          events.push({
+            name: this.rndElement(this.names),
+            color: this.rndElement(this.colors),
+            start,
+            end,
+            timed,
+          })
+        }
+        //this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
+      rndElement (arr) {
+        return arr[this.rnd(0, arr.length - 1)]
+      },
+       submit () {
+        this.$refs.observer.validate()
+      },
+      clear () {
+        this.courseName = ''
+        this.courseCredits = ''
+        this.difficultyLevel = ''
+        this.minimumHours = null
+        this.courseStartTime = null
+        this.courseEndTime = null
+        this.$refs.observer.reset()
+      },
+    },
+  }
+</script>
+<style scoped lang="scss">
+.v-event-draggable {
+  padding-left: 6px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.v-event-timed {
+  user-select: none;
+  -webkit-user-select: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.v-event-drag-bottom {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 4px;
+  height: 4px;
+  cursor: ns-resize;
+  &::after {
+    display: none;
+    position: absolute;
+    left: 50%;
+    height: 4px;
+    border-top: 1px solid white;
+    border-bottom: 1px solid white;
+    width: 16px;
+    margin-left: -8px;
+    opacity: 0.8;
+    content: '';
+  }
+  &:hover::after {
+    display: block;
+  }
 }
 </style>

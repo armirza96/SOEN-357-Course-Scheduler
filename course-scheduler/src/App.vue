9355a7c5b1b0c94d5e6e30ff0b9689457a6c25b2
Ahnaf Habib Khan <ahnafkhan70@gmail.com>
2022-04-16 12:43:55 -0400

    SignUP finished

--- course-scheduler/src/App.vue
+++ course-scheduler/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <LogIn/>
+      <SignUp/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import LogIn from './components/LogIn';
+// import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
 
 export default {
   name: 'App',
 
   components: {
     // HelloWorld,
-    LogIn,
+    SignUp,
   },
 
   data: () => ({

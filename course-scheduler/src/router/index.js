a4c9d43f67f224c6ff3eadb8a252ec14303bf9e8
Ahnaf Habib Khan <ahnafkhan70@gmail.com>
2022-04-17 23:19:30 -0400

    LogIn link changed

--- course-scheduler/src/router/index.js
+++ course-scheduler/src/router/index.js
@@ -3,7 +3,7 @@ import SignUp from "../components/SignUp.vue";
 import Calendar from "../components/Calendar.vue";
 
 export default [
-  { path: "/", component: Login },
+  { path: "/Login", component: Login },
   { path: "/SignUp", component: SignUp },
   { path: "/Calendar", component: Calendar },
 ];
